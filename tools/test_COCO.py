Add correct normalization factor for COCO

tools/test_COCO.py
@@ -63,30 +63,32 @@ if __name__ == '__main__':
 
     file_path = test_args.dataroot + "path_list.txt"
     with open(file_path, "r") as file:
-        for path in tqdm(file, total=get_num_lines(file_path)):     
+        for path in tqdm(file, total=get_num_lines(file_path)):
+            out_path = os.path.join(test_args.dataroot, 'VNL_Monocular', os.path.split(path)[1])
+            out_path = os.path.splitext(out_path)[0] + ".png"
+
+            # if not os.path.exists(out_path):
             with torch.no_grad():
-                img_path = path.strip()
-                img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+                img = Image.open(img_path)
                 if img is None:
                     print("Error loading: " + img_path)
                 else:
-                    img_resize = cv2.resize(img, (int(img.shape[1]), int(img.shape[0])), interpolation=cv2.INTER_LINEAR)
+                    img_resize = img.copy()
                     img_torch = scale_torch(img_resize, 255)
                     img_torch = img_torch[None, :, :, :].cuda()
 
-                    _, pred_depth_softmax= model.module.depth_model(img_torch)
+                    _, pred_depth_softmax = model.module.depth_model(img_torch)
                     pred_depth = bins_to_depth(pred_depth_softmax)
-                    pred_depth = pred_depth.cpu().numpy().squeeze()
-                    #pred_depth = (pred_depth / pred_depth.max() * 60000).astype(np.uint16)  # scale 60000 for visualization
-                    pred_depth_scaled = (pred_depth * 60000).astype(np.uint16)
 
-                    out_path = os.path.join(test_args.dataroot, 'VNL_Monocular', os.path.split(path)[1])
-                    out_path = os.path.splitext(out_path)[0] + ".png"
+                    # Un-normalize using factor from vnl/data/nyudv2_dataset.py
+                    pred_depth = 10.0 * pred_depth.cpu().numpy().squeeze()
 
-                    if np.any(pred_depth > 1.0):
-                        print("Possible clipping on: " + out_path)
-                    
-                    cv2.imwrite(out_path, pred_depth_scaled)
-                    #depth = Image.fromarray(pred_depth).convert("L")
-                    #with open(out_path, 'wb') as fp:
-                    #    depth.save(fp, "JPEG")
+                    # Convert to uint16 for storage
+                    pred_depth_scaled = (pred_depth * 256).astype('uint16')
+
+                    # Write out as uint16 png
+                    with open(out_path, 'wb') as f:
+                        writer = png.Writer(width=pred_depth.shape[1], height=pred_depth.shape[0], bitdepth=16,
+                                            greyscale=True)
+                        z = pred_depth_scaled.tolist()
+                        writer.write(f, z)